4152c280dce5ac505e9d3cdecbcef98cba7d8184
Kamurai <16156165+Kamurai@users.noreply.github.com>
2021-03-13 19:21:58 -0500

    Keyword Indices Assigned

--- _SQLServer/TableLoad_11_Booty.sql
+++ _SQLServer/TableLoad_11_Booty.sql
@@ -1,18 +1,18 @@
 --Table Load 4
 --1.0 Booty
---Boo Boody --25
+--Boo Boody --Index 25
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, Module, Mode, Flavor) VALUES ('Boo Booty',
 'http://htkb.info/SDE/1.0/Cards/Booty/Card_Boo_Booty_1.0.png', 
 'http://htkb.info/SDE/1.0/Cards/Booty/Card_Boo_Booty_1.0_back.png', 
 'http://htkb.online/SDWiki/SDE/1.0/Cards/Boo_Booty.xhtml', 
 'Booty', '1.0', '1.0 Core Set', 'Default',   
 'Fiendish constructs, the Boo Booty masquerades as a normal treasure chest.  When a greedy hero strays too near it springs to life, devouring them equipment and all.');
-insert into Characters (CardIndex) VALUES (2);
-insert into AffinityAssignments (CharacterIndex, AffinityIndex) VALUES (2, 1);
-insert into KeywordAssignments (CharacterIndex, KeywordIndex) VALUES (2, 2);
-insert into KeywordAssignments (CharacterIndex, KeywordIndex) VALUES (2, 3);
-insert into AbilityAssignments (CharacterIndex, AbilityIndex) VALUES (2, 4);
-insert into ExploreCharacters (CharacterIndex, ModelSize, CreatureType, Movement, Actions, Strength, Armor, Will, Dexterity, Health, Potions) VALUES (2, 'Small', 'Kodama', 3, 1, '2R', '2R', '1B', '1B', 2, 0);
-insert into OffenseAssignments (CharacterIndex, AttributeIndex, OffenseRange) VALUES (2,1,1);
-insert into DefenseAssignments (CharacterIndex, AttributeIndex) VALUES (2,2);
-insert into Monsters (ExploreCharacterIndex, Bits, Skulls) VALUES (2, 'Elite', 16, 1);
+insert into Characters (CardIndex) VALUES (25);
+insert into AffinityAssignments (ExploreCharacterIndex, AffinityIndex) VALUES (25, 1);
+insert into KeywordAssignments (ExploreCharacterIndex, KeywordIndex) VALUES (25, 2);
+insert into KeywordAssignments (ExploreCharacterIndex, KeywordIndex) VALUES (25, 3);
+insert into AbilityAssignments (ExploreCharacterIndex, AbilityIndex) VALUES (25, 4);
+insert into ExploreCharacters (CharacterIndex, Gender, ModelSize, CreatureType, Movement, Actions, Strength, Armor, Will, Dexterity, Health, Potions) VALUES (25, 'Either', 'Small', 'Kodama', 3, 1, '2R', '2R', '1B', '1B', 2, 0);
+insert into OffenseAssignments (ExploreCharacterIndex, AttributeIndex, OffenseRange) VALUES (25,1,1);
+insert into DefenseAssignments (ExploreCharacterIndex, AttributeIndex) VALUES (25,2);
+insert into Monsters (ExploreCharacterIndex, Bits, Skulls) VALUES (25, 'Elite', 16, 1);
