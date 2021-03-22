b21da820a3957cc32e9bdc1d0019c38b6bee30f1
Kamurai <16156165+Kamurai@users.noreply.github.com>
2021-03-22 08:37:03 -0400

    Test Adjustment for Heroes

--- SDWiki/src/java/SDE/Bean/ExploreCharacterBean.java
+++ SDWiki/src/java/SDE/Bean/ExploreCharacterBean.java
@@ -10,9 +10,12 @@ import SDE.ExploreCharacter;
 public class ExploreCharacterBean extends CardBean{
     private ExploreCharacter exploreCharacter;
     
+    private String displayPage;//TODO? REMOVE
+    
     public ExploreCharacterBean(){
         super();
         this.uni                = new Universal();
+        this.displayPage        = "";
         
         this.exploreCharacter   = new ExploreCharacter();        
     }
@@ -20,7 +23,17 @@ public class ExploreCharacterBean extends CardBean{
     public ExploreCharacter getExploreCharacter() {
         return exploreCharacter;
     }
+    
+    public String setDisplayPage(String link){
+        String path = uni.getAppPath()+link;
+        
+        this.exploreCharacter   = oneDAO.pullOneExploreCharacter(path);
+        this.displayPage        = uni.getAppPath()+"SDE/Content/ExploreCharacter.xhtml";
+        
+        return "./Layout.xhtml";
+    }
 
+    //TODO? REMOVE
     public String setExploreCharacterPage(String link){
         String path = uni.getAppPath()+link;
         
