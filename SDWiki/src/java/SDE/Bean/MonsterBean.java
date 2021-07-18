251051953971338aa6542bca68f360d2fa5ff9b6
Kamurai <16156165+Kamurai@users.noreply.github.com>
2021-07-18 19:53:50 +0000

    Navigation methods update

--- SDWiki/src/java/SDE/Bean/MonsterBean.java
+++ SDWiki/src/java/SDE/Bean/MonsterBean.java
@@ -4,28 +4,50 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import Main.Universal;
 import SDE.Monster;
+import java.util.ArrayList;
+import java.util.List;
 
 @ManagedBean(name="SDEMonsterBean")
 @RequestScoped
 public class MonsterBean extends CardBean{
     private Monster monster;
-    
+    private List<SDE.NavItem> monsterList;
+   
     public MonsterBean(){
         super();
         this.uni       = new Universal();
         
         this.monster   = new Monster();        
+        this.monsterList   = new ArrayList<SDE.NavItem>();
     }
 
     public Monster getMonster() {
         return monster;
     }
 
+    @Override
     public String setDisplayPage(String link){
         String path = uni.getAppPath()+link;
         
-        this.monster        = oneDAO.pullOneMonster(path);
+        if(path.compareTo("") != 0){
+            this.monster        = oneDAO.pullOneMonster(path);
+        }
         
         return "./Layout.xhtml";
+    }
+
+    public String setDisplayPage(String link, String version){
+        String path = uni.getAppPath()+link;
+        
+        if(path.compareTo("") != 0){
+            this.monster   = oneDAO.pullOneMonster(path);
+        }
+        this.monsterList  = navDAO.pullNavigationForMonsters(version);
+        
+        return "./Layout.xhtml";
+    }
+
+    public List<SDE.NavItem> getMonsterList() {
+        return monsterList;
     }
 }
