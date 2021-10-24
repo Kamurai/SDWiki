b6dd847f713cea81660ff5a79d230137918a54d8
Kamurai <16156165+Kamurai@users.noreply.github.com>
2021-10-24 20:23:33 +0000

    Added Spirit Walker flavor text

--- _SQLServer/Backup_FK/TableLoad_26_Arcade_Warbands.sql
+++ _SQLServer/Backup_FK/TableLoad_26_Arcade_Warbands.sql
@@ -1088,7 +1088,7 @@ insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, 
 'http://htkb.info/SDE/FK/Cards/Arcade/Warbands/Shallow_Grave/Card_Arcade_Warband_Shallow_Grave_Dust_Coven_FK_back.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Arcade/Warbands/Shallow_Grave/Dust_Coven.xhtml', 
 'Warband', 'FK', 'FK Core Set', 'Default', 
-'');
+'Spirit Walkers live between two worlds: the mortal realm and the spirit world.  It was they who first made contact with their trollish ancestors, seeking the wisdom of those who came before.');
 insert into Characters (CardIndex) VALUES (423);
 insert into ArcadeCharacters (CharacterIndex, SoloStatLineIndex, GangStatLineIndex) 
 VALUES (340, 143, 263);
