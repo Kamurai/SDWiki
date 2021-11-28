66b0d1602e5e380c37fde8bfc857763f9beda87b
Kamurai <16156165+Kamurai@users.noreply.github.com>
2021-11-28 02:34:45 +0000

    Arcade Booty Fix

--- _SQLServer/TableLoad_B_FK.sql
+++ _SQLServer/TableLoad_B_FK.sql
@@ -5664,8 +5664,8 @@ insert into Monsters (ExploreCharacterIndex, RankType, Bits, Skulls) VALUES (248
 --FK Arcade Booty
 --Boody Biter	--CardIndex 334	--CharacterIndex 249	--ArcadeCharacterIndex 1
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor) VALUES ('Booty Biter',
-'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Booty_Biter_FK.png', 
-'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Booty_Biter_FK_back.png', 
+'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Booty_Booty_Biter_FK.png', 
+'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Booty_Booty_Biter_FK_back.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Arcade/Booty/Booty_Biter.xhtml', 
 'Booty', 'FK', 'FK Core Set', 'Default', 
 'Boo booty are the unfortunate creation of gnomish tinkering, paired with dwarven greed.  Originally intended to guard the treasure vaults of the Hearthsworn Dwarves, the spirit of the Dark Consul filled the constructs with a malevolent intellect.  Turning upon their former masters, boo booty rabildly guard their treasure from all who would seek to claim it.');
@@ -5680,8 +5680,8 @@ insert into KeywordAssignments (GangMemberIndex, KeywordIndex) VALUES (1, 466); 
 
 --Bouncing Booty --CardIndex 335	--CharacterIndex 250	--ArcadeCharacterIndex 2
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor) VALUES ('Bouncing Booty',
-'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Bouncing_Booty_FK.png', 
-'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Bouncing_Booty_FK_back.png', 
+'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Booty_Bouncing_Booty_FK.png', 
+'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Booty_Bouncing_Booty_FK_back.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Arcade/Booty/Bouncing_Booty.xhtml', 
 'Booty Grinder', 'FK', 'FK Core Set', 'Default', 
 'While the original function of boo booty are to guard their treasure, Booty Grinders have the unfortunate tendency to devour it instead.  Once a Booty Grinder has caught the scent of shining steel, lustrous gold, or sparkling gems it will doggedly pursue any who carry them like a ravenous hound.');
@@ -5696,8 +5696,8 @@ insert into AbilityAssignments (GangMemberIndex, AbilityIndex) VALUES (2, 197); 
 
 --Booty Grinder --CardIndex 336	--CharacterIndex 251	--ArcadeCharacterIndex 3
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor) VALUES ('Booty Grinder',
-'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Booty_Grinder_FK.png', 
-'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Booty_Grinder_FK_back.png', 
+'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Booty_Booty_Grinder_FK.png', 
+'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Booty_Booty_Grinder_FK_back.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Arcade/Booty/Booty_Grinder.xhtml', 
 'Booty', 'FK', 'FK Core Set', 'Default', 
 'While the original function of boo booty are to guard their treasure, Booty Grinders have the unfortunate tendency to devour it instead.  Once a Booty Grinder has caught the scent of shining steel, lustrous gold, or sparkling gems it will doggedly pursue any who carry them like a ravenous hound.');
