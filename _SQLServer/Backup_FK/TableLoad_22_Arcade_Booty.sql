b33030419779c800a4996e91b4b7e8cf2602ffaa
Kamurai <16156165+Kamurai@users.noreply.github.com>
2021-11-28 04:38:32 +0000

    Booty Fix

--- _SQLServer/Backup_FK/TableLoad_22_Arcade_Booty.sql
+++ _SQLServer/Backup_FK/TableLoad_22_Arcade_Booty.sql
@@ -20,7 +20,7 @@ insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, 
 'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Bouncing_Booty_FK.png', 
 'http://htkb.info/SDE/FK/Cards/Arcade/Booty/Card_Bouncing_Booty_FK_back.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Arcade/Booty/Bouncing_Booty.xhtml', 
-'Booty Grinder', 'FK', 'FK Core Set', 'Default', 
+'Booty', 'FK', 'FK Core Set', 'Default', 
 'While the original function of boo booty are to guard their treasure, Booty Grinders have the unfortunate tendency to devour it instead.  Once a Booty Grinder has caught the scent of shining steel, lustrous gold, or sparkling gems it will doggedly pursue any who carry them like a ravenous hound.');
 insert into Characters (CardIndex) VALUES (335);
 insert into AffinityAssignments (CharacterIndex, AffinityIndex) VALUES (250, 1); --Citrine
