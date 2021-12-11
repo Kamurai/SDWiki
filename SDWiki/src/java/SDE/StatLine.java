b1370fff83313222b66605347da1a42b869511d3
Kamurai <16156165+Kamurai@users.noreply.github.com>
2021-12-11 01:13:48 +0000

    Added StatLine methods

--- SDWiki/src/java/SDE/StatLine.java
+++ SDWiki/src/java/SDE/StatLine.java
@@ -21,4 +21,35 @@ public class StatLine {
         this.range       = range;
     }
     
+    public boolean validate(){
+        boolean result = false;
+        
+        result = (this.action > -1) && (this.strength > -1) && (this.range > -1);
+        
+        return result;
+    }
+
+    public int getAction() {
+        return action;
+    }
+
+    public void setAction(int action) {
+        this.action = action;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
 }
