56545ae8b41d1cdfceb67c9021bbae45fa0ec2e7
Kamurai <16156165+Kamurai@users.noreply.github.com>
2022-01-30 16:19:03 +0000

    Added individual DbInfo classes

---
+++ SDAPI/src/Database/SDE/SDEDbInfo.java
@@ -0,0 +1,34 @@
+package Database.SDE;
+
+import Database.*;
+
+public class SDEDbInfo extends DbInfo{
+    private static String driver = "com.microsoft.jdbc.sqlserver.SQLServerDriver";
+    public static String getDriver(){
+        return driver;
+    }
+    private String url         = "jdbc:sqlserver://localhost";
+    public String getUrl(){
+        return url;
+    }
+    private String dbName      = ";DatabaseName=SDE";
+    public String getDbName(){
+        return dbName;
+    }
+    private String dbUsername  = "sa";
+    public String getDbUsername(){
+        return dbUsername;
+    }
+    private String dbPassword  = "yellow23";
+    public String getDbPassword(){
+        return dbPassword;
+    }
+    
+    public SDEDbInfo(){
+        driver      = "com.microsoft.jdbc.sqlserver.SQLServerDriver";
+        url         = "jdbc:sqlserver://localhost";
+        dbName      = ";DatabaseName=SDE";
+        dbUsername  = "sa";
+        dbPassword  = "yellow23";
+    }
+}
