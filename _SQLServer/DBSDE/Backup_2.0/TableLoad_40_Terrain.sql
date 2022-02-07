8b1a0bd7d102e9bd374d9c4fbbe4840c5be4e131
Pennie-K <79235064+Pennie-K@users.noreply.github.com>
2022-02-07 13:38:14 -0600

    2.0 Terrain updated

--- _SQLServer/DBSDE/Backup_2.0/TableLoad_40_Terrain.sql
+++ _SQLServer/DBSDE/Backup_2.0/TableLoad_40_Terrain.sql
@@ -1,12 +1,149 @@
 --2.0 Utility Cards
---Terrain
+--Chasm --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Chasm',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Chasm_2.0_DI.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Chasm.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, ''); 
+insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Chasm
+
+--Chasm2 --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Chasm2',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Chasm2_2.0.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Chasm2.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, ''); 
+insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Chasm
+
 --Difficult Terrain --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES ('Difficult Terrain',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Difficult_Terrain_2.0.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Difficult_Terrain.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, ''); 
+insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Difficult Terrain
+
+--Difficult Terrain DI --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Difficult Terrain DI',
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/DI/Card_Terrain_Difficult_Terrain_2.0_DI.png', 
-'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_2.0_back.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Difficult_Terrain_DI.xhtml', 
 'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, ''); 
 insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Difficult Terrain
+
+--Difficult Terrain2 --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Difficult Terrain2',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Difficult_Terrain2_2.0.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Difficult_Terrain2.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, ''); 
+insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Difficult Terrain
+
+--Fire DI--CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Fire',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/DI/Card_Terrain_Fire_2.0_DI.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Fire_DI.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, 'A model which enters one or more Fire squares suffers fire at the end of the current activation.'); 
+insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Fire
+
+--Goddess --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Goddess',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Goddess_2.0.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Goddess.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, 'Models that end their activation adjacent to a Goddess structure may remove one wound token.'); 
+insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Blessing
+insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Structure
+
+--Lava --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Lava',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Lava_2.0.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Lava.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, 'A model which enters one or more Lava squares suffers fire at the end of the current activation.'); 
+insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Fire
+
+--Lava 1 --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Lava 1',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/DP/Card_Terrain_Lava_1_2.0.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Lava_1.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, 'A model which enters one or more Lava squares suffers fire at the end of the current activation.'); 
+insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Fire
+
+--Spawn Location --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Spawn Location',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Spawn_Location_2.0.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Spawn_Location.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, 'Spawning points are placed in spawn location squares.'); 
+
+--Spawn Location DI  --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Spawn Location',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/DI/Card_Terrain_Spawn_Location_2.0_DI.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Spawn_Location_DI.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, 'Spawning points are placed in Spawn Location squares.'); 
+
+--Structure --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Structure',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Structure_2.0.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Structure.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, ''); 
+insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Structure
+
+--Structure DI  --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Structure',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/DI/Card_Terrain_Structure_2.0_DI.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Structure_DI.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, ''); 
+insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Structure
+
+--Structure2 --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Structure2',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Structure2_2.0.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Structure2.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, ''); 
+insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (uXXX, 589); --Structure
+
+--Treasure Pile DI --CardIndex cXXX --UtilityIndex uXXX --TerrainIndex tXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
+VALUES ('Treasure Pile',
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/DI/Card_Terrain_Treasure_Pile_2.0_DI.png', 
+'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Treasure_Pile_DI.xhtml', 
+'Terrain', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+insert into Utilities (CardIndex, UtilityDescription) VALUES (cXXX, 'If there are one or more Heroes on a Treasure Pile Square during the Hero Power-Up phase, the party may trade four loot cards from the Backpack for a treasure instead of the usual six loot cards.'); 
 
