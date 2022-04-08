877bf0e8c1cf6baa724133a343d7342afb9e26ab
Kamurai <16156165+Kamurai@users.noreply.github.com>
2022-04-08 00:16:01 +0000

    2.0.2 Equipment update

--- _SQLServer/DBSDE/Backup_2.0.2/TableLoad_31_Equipment.sql
+++ _SQLServer/DBSDE/Backup_2.0.2/TableLoad_31_Equipment.sql
@@ -1,3 +1,37 @@
 --2.0.2 
 --Equipment
---Accursed Longsword --CardIndex cXXX --EquipmentIndex eXXX
+--Barbarian Chic --CardIndex cXXX --EquipmentIndex eXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor) VALUES (
+'Barbarian Chic',
+'http://htkb.info/SDE/2.0/Cards/Treasure/Specific/Card_Treasure_Barbarian_Chic_2.0.png', 
+'http://htkb.info/SDE/2.0/Cards/Treasure/Card_Treasure_Back_2.0.png',
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Treasure/Specific/Barbarian_Chic.xhtml', 
+'Treasure', '2.0', '2.0 Core Set', 'Default',
+'The very latest thing.');
+insert into Equipment (CardIndex, EquipmentType, Position, Effect) VALUES (cXXX, 'Character Treasure', 'Right', '+1G ARM');
+insert into KeywordAssignments (EquipmentIndex, KeywordIndex) VALUES (eXXX, kXXX); --Brutality Aura
+
+--Branch Blade --CardIndex cXXX --EquipmentIndex eXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor) VALUES (
+'Branch Blade',
+'http://htkb.info/SDE/2.0/Cards/Treasure/Specific/Card_Treasure_Branch_Blade_2.0.png', 
+'http://htkb.info/SDE/2.0/Cards/Treasure/Card_Treasure_Back_2.0.png',
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Treasure/Specific/Branch_Blade.xhtml', 
+'Treasure', '2.0', '2.0 Core Set', 'Default',
+'');
+insert into Equipment (CardIndex, EquipmentType, Position, Effect) VALUES (cXXX, 'Character Treasure', 'Left', '+1R STR');
+insert into KeywordAssignments (EquipmentIndex, KeywordIndex) VALUES (eXXX, kXXX); --Counterstrike
+
+--Burlap Sack --CardIndex cXXX --EquipmentIndex eXXX
+insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor) VALUES (
+'Burlap Sack',
+'http://htkb.info/SDE/2.0/Cards/Treasure/Specific/Card_Treasure_Burlap_Sack_2.0.png', 
+'http://htkb.info/SDE/2.0/Cards/Treasure/Card_Treasure_Back_2.0.png',
+'http://htkb.online/SDWiki/SDE/2.0/Cards/Treasure/Specific/Burlap_Sack.xhtml', 
+'Treasure', '2.0', '2.0 Core Set', 'Default',
+'"Look at all the stuff you can put in here!"');
+insert into Equipment (CardIndex, EquipmentType, Position, Effect) VALUES (cXXX, 'Character Treasure', 'Bottom', '+1R STR +1R ARM +1R WILL +1R DEX');
+
+
+
+
