672a9fa58c5ae5353e98b5f50f895a63f082cc39
Kamurai <16156165+Kamurai@users.noreply.github.com>
2022-04-30 05:27:19 +0000

    DBNAS: Established DB structure and examples

---
+++ _SQLServer/DBNAS/TableLoad_03_Teams.sql
@@ -0,0 +1,80 @@
+--Table Load 20
+--NAS
+--Teams
+
+--Air Shrine Clan --CardIndex cXXX --TeamIndex 0
+insert into Teams (TeamName, Quickplay)
+values('Air Shrine Clan', '1x Chunin<br>1x Madoushi<br>2x Kunoichi<br>2x Yajiri<br>3x Kaiken');
+insert into AffinityAssignments (TeamIndex, AffinityIndex) VALUES (0, 0); --Air
+insert into KeywordAssignments (TeamIndex, KeywordIndex) VALUES (0, kXXX); --Combat Results
+
+--Earth Shrine Clan --CardIndex cXXX --TeamIndex 1
+insert into Teams (TeamName, Quickplay)
+values('Earth Shrine Clan', '1x Chunin<br>1x Madoushi<br>2x Kunoichi<br>2x Yajiri<br>3x Kaiken');
+insert into AffinityAssignments (TeamIndex, AffinityIndex) VALUES (1, 1); --Earth
+insert into KeywordAssignments (TeamIndex, KeywordIndex) VALUES (1, kXXX); --Combat Results
+
+--Fire Shrine Clan --CardIndex cXXX --TeamIndex 2
+insert into Teams (TeamName, Quickplay)
+values('Fire Shrine Clan', '1x Chunin<br>1x Madoushi<br>2x Kunoichi<br>2x Yajiri<br>3x Kaiken');
+insert into AffinityAssignments (TeamIndex, AffinityIndex) VALUES (2, 2); --Fire
+insert into KeywordAssignments (TeamIndex, KeywordIndex) VALUES (2, kXXX); --Combat Results
+
+--Spirit Shrine Clan --CardIndex cXXX --TeamIndex 3
+insert into Teams (TeamName, Quickplay)
+values('Spirit Shrine Clan', '1x Chunin<br>1x Madoushi<br>2x Kunoichi<br>2x Yajiri<br>3x Kaiken');
+insert into AffinityAssignments (TeamIndex, AffinityIndex) VALUES (3, 3); --Spirit
+insert into KeywordAssignments (TeamIndex, KeywordIndex) VALUES (3, kXXX); --Combat Results
+
+--Void Shrine Clan --CardIndex cXXX --TeamIndex 4
+insert into Teams (TeamName, Quickplay)
+values('Void Shrine Clan', '1x Chunin<br>1x Madoushi<br>2x Kunoichi<br>2x Yajiri<br>3x Kaiken');
+insert into AffinityAssignments (TeamIndex, AffinityIndex) VALUES (4, 4); --Void
+insert into KeywordAssignments (TeamIndex, KeywordIndex) VALUES (4, kXXX); --Combat Results
+
+--Water Shrine Clan --CardIndex cXXX --TeamIndex 5
+insert into Teams (TeamName, Quickplay)
+values('Water Shrine Clan', '1x Chunin<br>1x Madoushi<br>2x Kunoichi<br>2x Yajiri<br>3x Kaiken');
+insert into AffinityAssignments (TeamIndex, AffinityIndex) VALUES (5, 5); --Water
+insert into KeywordAssignments (TeamIndex, KeywordIndex) VALUES (5, kXXX); --Combat Results
+
+--Ijin Clan --CardIndex cXXX --TeamIndex 6
+insert into Teams (TeamName, Quickplay)
+values('Ijin Clan', '');
+insert into AffinityAssignments (TeamIndex, AffinityIndex) VALUES (6, 4); --Void
+
+--Ika Clan --CardIndex cXXX --TeamIndex 7
+insert into Teams (TeamName, Quickplay)
+values('Ika Clan', '');
+insert into AffinityAssignments (TeamIndex, AffinityIndex) VALUES (7, 5); --Water
+
+--Kitsune Clan --CardIndex cXXX --TeamIndex 6
+insert into Teams (TeamName, Quickplay)
+values('Kitsune Clan', '');
+insert into AffinityAssignments (TeamIndex, AffinityIndex) VALUES (8, 3); --Spirit
+
+--Tanchyo Clan --CardIndex cXXX --TeamIndex 6
+insert into Teams (TeamName, Quickplay)
+values('Tanchyo Clan', '');
+insert into AffinityAssignments (TeamIndex, AffinityIndex) VALUES (9, 0); --Air
+
+--Tora Clan --CardIndex cXXX --TeamIndex 6
+insert into Teams (TeamName, Quickplay)
+values('Tora Clan', '');
+insert into AffinityAssignments (TeamIndex, AffinityIndex) VALUES (10, 2); --Fire
+
+--Yamazaru Clan --CardIndex cXXX --TeamIndex 6
+insert into Teams (TeamName, Quickplay)
+values('Yamazaru Clan', '');
+insert into AffinityAssignments (TeamIndex, AffinityIndex) VALUES (11, 1); --Earth
+
+
+
+
+
+
+
+
+
+
+
--- _SQLServer/DBNAS/_Tables.sql
+++ _SQLServer/DBNAS/_Tables.sql
@@ -15,7 +15,10 @@ insert into Attributes (Attribute) VALUES ('Defense'); --2
 insert into Attributes (Attribute) VALUES ('Koban'); --3
 
 --Keywords
-create table Keywords (KeywordIndex bigint IDENTITY(0,1) PRIMARY KEY, KeywordVersion varchar(20) not null, KeywordType varchar(20) not null, KeywordName varchar(50) not null, KeywordDescription varchar(2500) not null);
+create table Keywords (KeywordIndex bigint IDENTITY(0,1) PRIMARY KEY, KeywordVersion varchar(20) not null, KeywordType varchar(20) not null, KeywordName varchar(50) not null, KeywordDescription varchar(1000) not null);
+
+--Teams
+create table Teams (TeamIndex bigint IDENTITY(0,1) PRIMARY KEY, TeamName varchar(20) not null, Quickplay varchar(100) not null);
 
 --Cards
 create table Cards (CardIndex bigint IDENTITY(0,1) PRIMARY KEY, CardName varchar(50) not null, PictureFront varchar(150) not null, PictureBack varchar(150) not null, Link varchar(125) not null, CardType varchar(20) not null, ProductSet varchar(20) not null, ProductModule varchar(20) not null, Flavor varchar(1250));
@@ -23,14 +26,15 @@ create table Cards (CardIndex bigint IDENTITY(0,1) PRIMARY KEY, CardName varchar
 --Shinobi
 create table Shinobi (ShinobiIndex bigint IDENTITY(0,1) PRIMARY KEY, CardIndex bigint not null, StandieFront varchar(150) not null, StandieBack varchar(150) not null, Gender varchar(50) not null, ModelSize varchar(50) not null, Affiliation varchar(50) not null, RankType varchar(50) not null, Movement int not null, Attack int not null, Defense varchar(50) not null, Koban varchar(50) not null);
 
-create table AffinityAssignments (AffinityAssignmentIndex bigint IDENTITY(0,1) PRIMARY KEY, AffinityIndex bigint not null, ShinobiIndex bigint not null);
-
 --Moon Cards
-create table MoonCards (MoonCardIndex bigint IDENTITY(0,1) PRIMARY KEY, CardIndex bigint not null, MoonCardCondition varchar(2500) not null, MoonCardAffinityMatch varchar(2500) not null );
+create table MoonCards (MoonCardIndex bigint IDENTITY(0,1) PRIMARY KEY, CardIndex bigint not null, MoonCardCondition varchar(1000) not null, MoonCardAffinityMatch varchar(1000) not null );
 
 create table Utilities (UtilityIndex bigint IDENTITY(0,1) PRIMARY KEY, CardIndex bigint not null, UtilityDescription varchar(1000) not null);
 
-create table KeywordAssignments (KeywordAssignmentIndex bigint IDENTITY(0,1) PRIMARY KEY, KeywordIndex bigint not null, ShinobiIndex bigint, MoonCardIndex bigint, UtilityIndex bigint);
+--Assignments
+create table AffinityAssignments (AffinityAssignmentIndex bigint IDENTITY(0,1) PRIMARY KEY, AffinityIndex bigint not null, ShinobiIndex bigint, TeamIndex bigint, MoonCardIndex bigint);
+create table KeywordAssignments (KeywordAssignmentIndex bigint IDENTITY(0,1) PRIMARY KEY, KeywordIndex bigint not null, ShinobiIndex bigint, TeamIndex bigint, MoonCardIndex bigint, UtilityIndex bigint);
+create table TeamAssignments (TeamAssignmentIndex bigint IDENTITY(0,1) PRIMARY KEY, TeamIndex bigint not null, ShinobiIndex bigint not null);
 
 
 
