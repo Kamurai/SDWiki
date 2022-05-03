abfc4c130e9fb83c004b1642ae202bb43ceef382
Kamurai <16156165+Kamurai@users.noreply.github.com>
2022-05-03 03:00:27 +0000

    DBRRI: Lawbots and High Noon updates

--- _SQLServer/DBRRI/TableLoad_31_High_Noon.sql
+++ _SQLServer/DBRRI/TableLoad_31_High_Noon.sql
@@ -2,18 +2,120 @@
 --RRI
 --High Noon Cards
 
---Both Barrels --CardIndex cXXX --HighNoonCardIndex 0
-insert into Cards (CardIndex, CardName, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, Flavor, Lore) 
-VALUES (34, 'Both Barrels', 
+--Both Barrels --CardIndex 34 --HighNoonCardIndex 0
+insert into Cards (CardName, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, Flavor, Lore) 
+VALUES ('Both Barrels', 
 'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Both_Barrels.png', 
 'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Back.png', 
 'http://htkb.online/SDWiki/RRI/Cards/High_Noon/Both_Barrels.xhtml', 
 'High Noon', 'RRI', 'RRI Core Set', 'Default', 
-'', 
+'"It''s all or nothin''!"', 
 ''
 );
 insert into HighNoonCards (CardIndex) VALUES (34);
-insert into KeywordAssignments (KeywordIndex, HighNoonCardIndex) VALUES (34, 0); --Both Barrels
+insert into KeywordAssignments (HighNoonCardIndex, KeywordIndex) VALUES (0, 35); --Both Barrels
+
+--Crossfire --CardIndex 35 --HighNoonCardIndex 1
+insert into Cards (CardName, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, Flavor, Lore) 
+VALUES ('Crossfire', 
+'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Crossfire.png', 
+'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Back.png', 
+'http://htkb.online/SDWiki/RRI/Cards/High_Noon/Crossfire.xhtml', 
+'High Noon', 'RRI', 'RRI Core Set', 'Default', 
+'"Wait, where did you come from ?!"', 
+''
+);
+insert into HighNoonCards (CardIndex) VALUES (35);
+insert into KeywordAssignments (HighNoonCardIndex, KeywordIndex) VALUES (1, 36); --Crossfire
+
+--Daring Acrobatics --CardIndex 36 --HighNoonCardIndex 2
+insert into Cards (CardName, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, Flavor, Lore) 
+VALUES ('Daring Acrobatics', 
+'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Daring_Acrobatics.png', 
+'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Back.png', 
+'http://htkb.online/SDWiki/RRI/Cards/High_Noon/Daring_Acrobatics.xhtml', 
+'High Noon', 'RRI', 'RRI Core Set', 'Default', 
+'"Time for some fancy moves!"', 
+''
+);
+insert into HighNoonCards (CardIndex) VALUES (36);
+insert into KeywordAssignments (HighNoonCardIndex, KeywordIndex) VALUES (2, 37); --Daring Acrobatics
+
+--Dead Man's Hand --CardIndex 37 --HighNoonCardIndex 3
+insert into Cards (CardName, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, Flavor, Lore) 
+VALUES ('Dead Man''s Hand', 
+'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Dead_Mans_Hand.png', 
+'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Back.png', 
+'http://htkb.online/SDWiki/RRI/Cards/High_Noon/Dead_Mans_Hand.xhtml', 
+'High Noon', 'RRI', 'RRI Core Set', 'Default', 
+'"Time for some fancy moves!"', 
+''
+);
+insert into HighNoonCards (CardIndex) VALUES (37);
+insert into KeywordAssignments (HighNoonCardIndex, KeywordIndex) VALUES (3, 38); --Dead Man's Hand
+
+--Dead Or Alive --CardIndex 38 --HighNoonCardIndex 4
+insert into Cards (CardName, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, Flavor, Lore) 
+VALUES ('Dead Or Alive', 
+'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Dead_Or_Alive.png', 
+'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Back.png', 
+'http://htkb.online/SDWiki/RRI/Cards/High_Noon/Dead_Or_Alive.xhtml', 
+'High Noon', 'RRI', 'RRI Core Set', 'Default', 
+'"Either way, I get paid."', 
+''
+);
+insert into HighNoonCards (CardIndex) VALUES (38);
+insert into KeywordAssignments (HighNoonCardIndex, KeywordIndex) VALUES (4, 39); --Dead Or Alive
+
+--Fan the Hammer --CardIndex 39 --HighNoonCardIndex 5
+insert into Cards (CardName, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, Flavor, Lore) 
+VALUES ('Fan the Hammer', 
+'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Fan_the_Hammer.png', 
+'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Back.png', 
+'http://htkb.online/SDWiki/RRI/Cards/High_Noon/Fan_the_Hammer.xhtml', 
+'High Noon', 'RRI', 'RRI Core Set', 'Default', 
+'"She shot all six Tinstars down before any one of ''em pulled their triggers!"', 
+''
+);
+insert into HighNoonCards (CardIndex) VALUES (39);
+insert into KeywordAssignments (HighNoonCardIndex, KeywordIndex) VALUES (5, 40); --Fan the Hammer
+
+--Fire Main Thrusters --CardIndex 40 --HighNoonCardIndex 6
+insert into Cards (CardName, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, Flavor, Lore) 
+VALUES ('Fire Main Thrusters', 
+'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Fire_Main_Thrusters.png', 
+'http://htkb.info/RRI/Cards/High_Noon/Card_HighNoon_Back.png', 
+'http://htkb.online/SDWiki/RRI/Cards/High_Noon/Fire_Main_Thrusters.xhtml', 
+'High Noon', 'RRI', 'RRI Core Set', 'Default', 
+'"Is it just me, or is this train moving faster?"', 
+''
+);
+insert into HighNoonCards (CardIndex) VALUES (40);
+insert into KeywordAssignments (HighNoonCardIndex, KeywordIndex) VALUES (6, 41); --Fire Main Thrusters
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
