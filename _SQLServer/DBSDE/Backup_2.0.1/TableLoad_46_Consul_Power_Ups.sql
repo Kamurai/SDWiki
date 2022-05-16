4f67d708444067db97e151bd60d396ce83248868
Pennie-K <79235064+Pennie-K@users.noreply.github.com>
2022-05-16 17:05:10 -0500

    2.0 Consul Power Ups indexed

--- _SQLServer/DBSDE/Backup_2.0.1/TableLoad_46_Consul_Power_Ups.sql
+++ _SQLServer/DBSDE/Backup_2.0.1/TableLoad_46_Consul_Power_Ups.sql
@@ -1,73 +1,73 @@
 --2.0 Consul Power Up Cards
---Fight Through the Pain	--CardIndex cXXX --ConsulPowerUpIndex cpuXXX
+--Fight Through the Pain	--CardIndex 3217 --ConsulPowerUpIndex 0
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES ('Fight Through the Pain',
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Fight_Through_the_Pain_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Consul_Power_Ups/Fight_Through_the_Pain.xhtml', 
 'Consul Power Up', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
-insert into Utilities (CardIndex, UtilityDescription,) VALUES (cXXX, 'Play this card at the start of a scenario.  All Ijin Oni and Ijin Kunoichi gain Sturdy in this scenario.');
+insert into Utilities (CardIndex, UtilityDescription,) VALUES (3217, 'Play this card at the start of a scenario.  All Ijin Oni and Ijin Kunoichi gain Sturdy in this scenario.');
 
---From Every Shadow	--CardIndex cXXX --ConsulPowerUpIndex cpuXXX
+--From Every Shadow	--CardIndex 3218 --ConsulPowerUpIndex 1
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES ('From Every Shadow',
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_From_Every_Shadow_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Consul_Power_Ups/From_Every_Shadow.xhtml', 
 'Consul Power Up', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
-insert into Utilities (CardIndex, UtilityDescription,) VALUES (cXXX, 'Play this card at the start of a scenario.  All Ijin Kunoichi gain +1AC during this scenario.');
+insert into Utilities (CardIndex, UtilityDescription,) VALUES (3218, 'Play this card at the start of a scenario.  All Ijin Kunoichi gain +1AC during this scenario.');
 
---Ijin Battle Cry	--CardIndex cXXX --ConsulPowerUpIndex cpuXXX
+--Ijin Battle Cry	--CardIndex 3219 --ConsulPowerUpIndex 2
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES ('Ijin Battle Cry',
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Ijin_Battle_Cry_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Consul_Power_Ups/Ijin_Battle_Cry.xhtml', 
 'Consul Power Up', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
-insert into Utilities (CardIndex, UtilityDescription,) VALUES (cXXX, 'Play this card at the start of any Consul Turn.  All monsters with Rampage gain red dice instead of blue dice from the Rampage ability for the duration of the turn.');
+insert into Utilities (CardIndex, UtilityDescription,) VALUES (3219, 'Play this card at the start of any Consul Turn.  All monsters with Rampage gain red dice instead of blue dice from the Rampage ability for the duration of the turn.');
 
---Poison Storm	--CardIndex cXXX --ConsulPowerUpIndex cpuXXX
+--Poison Storm	--CardIndex 3220 --ConsulPowerUpIndex 3
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES ('Poison Storm',
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Poison_Storm_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Consul_Power_Ups/Poison_Storm.xhtml', 
 'Consul Power Up', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
-insert into Utilities (CardIndex, UtilityDescription,) VALUES (cXXX, 'Play this card at any time during the Consul''s turn. All Ijin Yajiri perform a basic Missile Attack action with a +1G DEX bonus to the roll.');
+insert into Utilities (CardIndex, UtilityDescription,) VALUES (3220, 'Play this card at any time during the Consul''s turn. All Ijin Yajiri perform a basic Missile Attack action with a +1G DEX bonus to the roll.');
 
---Smashening, The	--CardIndex cXXX --ConsulPowerUpIndex cpuXXX
+--Smashening, The	--CardIndex 3221 --ConsulPowerUpIndex 4
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES ('Smashening, The',
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Smashening_The_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Consul_Power_Ups/Smashening_The.xhtml', 
 'Consul Power Up', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
-insert into Utilities (CardIndex, UtilityDescription,) VALUES (cXXX, 'Play this card at any time during the Consul''s turn. All Ijin Oni perform a basic Melee Attack action with a +1G STR bonus to the roll.');
+insert into Utilities (CardIndex, UtilityDescription,) VALUES (3221, 'Play this card at any time during the Consul''s turn. All Ijin Oni perform a basic Melee Attack action with a +1G STR bonus to the roll.');
 
---Strength of the Ijin	--CardIndex cXXX --ConsulPowerUpIndex cpuXXX
+--Strength of the Ijin	--CardIndex 3222 --ConsulPowerUpIndex 5
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES ('Strength of the Ijin',
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Strength_of_the_Ijin_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Consul_Power_Ups/Strength_of_the_Ijin.xhtml', 
 'Consul Power Up', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
-insert into Utilities (CardIndex, UtilityDescription,) VALUES (cXXX, 'Play this card at the start of a scenario.  All monsters gain +1B STR during this scenario.');
+insert into Utilities (CardIndex, UtilityDescription,) VALUES (3222, 'Play this card at the start of a scenario.  All monsters gain +1B STR during this scenario.');
 
---Surprise Attack!	--CardIndex cXXX --ConsulPowerUpIndex cpuXXX
+--Surprise Attack!	--CardIndex 3223 --ConsulPowerUpIndex 6
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES ('Surprise Attack!',
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Surprise_Attack_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Consul_Power_Ups/Surprise_Attack.xhtml', 
 'Consul Power Up', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
-insert into Utilities (CardIndex, UtilityDescription,) VALUES (cXXX, 'Play this card at any time during the Consul''s turn. Spawn four Ijin Kaiken anywhere in the dungeon. These kaiken may not move the turn they are spawned, but they may use offensive and support actions.');
+insert into Utilities (CardIndex, UtilityDescription,) VALUES (3223, 'Play this card at any time during the Consul''s turn. Spawn four Ijin Kaiken anywhere in the dungeon. These kaiken may not move the turn they are spawned, but they may use offensive and support actions.');
 
---Wrath of the Ijin	--CardIndex cXXX --ConsulPowerUpIndex cpuXXX
+--Wrath of the Ijin	--CardIndex 3224 --ConsulPowerUpIndex 7
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES ('Wrath of the Ijin',
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Wrath_of_the_Ijin_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Consul_Power_Ups/Card_Consul_Power_Up_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Consul_Power_Ups/Wrath_of_the_Ijin.xhtml', 
 'Consul Power Up', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
-insert into Utilities (CardIndex, UtilityDescription,) VALUES (cXXX, 'Play this card at the start of a scenario.  All monsters gain +1B to offense rolls during this scenario.');
+insert into Utilities (CardIndex, UtilityDescription,) VALUES (3224, 'Play this card at the start of a scenario.  All monsters gain +1B to offense rolls during this scenario.');
 
