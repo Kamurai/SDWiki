8a68829308781fef8454bf076e5f2b27b25fdf53
Pennie-K <79235064+Pennie-K@users.noreply.github.com>
2022-07-02 21:31:57 -0500

    2.0 Equipment update

--- _SQLServer/DBSDE/TableLoad_C_2.0.1.sql
+++ _SQLServer/DBSDE/TableLoad_C_2.0.1.sql
@@ -24058,7 +24058,7 @@ VALUES ('Difficult Terrain DI',
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/DI/Card_Terrain_Difficult_Terrain_2.0_DI.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Difficult_Terrain_DI.xhtml', 
-'Terrain', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
+'Terrain', '2.0 DI', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (1999, ''); 
 insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (16, 1100); --Difficult Terrain
 
@@ -24078,7 +24078,7 @@ VALUES ('Fire',
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/DI/Card_Terrain_Fire_2.0_DI.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Fire_DI.xhtml', 
-'Terrain', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
+'Terrain', '2.0 DI', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2001, ''); 
 insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (18, 1101); --Fire
 
@@ -24110,7 +24110,7 @@ VALUES ('Lava 1',
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/DP/Card_Terrain_Lava_1_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Lava_1.xhtml', 
-'Terrain', '2.0 DI', '2.0 Dragon''s Peak', 'Default', '');
+'Terrain', '2.0', '2.0 DP', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2004, ''); 
 insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (21, 1103); --Lava
 
@@ -24130,7 +24130,7 @@ VALUES ('Spawn Location',
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/DI/Card_Terrain_Spawn_Location_2.0_DI.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Spawn_Location_DI.xhtml', 
-'Terrain', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
+'Terrain', '2.0 DI', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2006, ''); 
 insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (23, 1104); --Spawn Location
 
@@ -24150,7 +24150,7 @@ VALUES ('Structure',
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/DI/Card_Terrain_Structure_2.0_DI.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Structure_DI.xhtml', 
-'Terrain', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
+'Terrain', '2.0 DI', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2008, ''); 
 insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (25, 1017); --Structure
 
@@ -24170,7 +24170,7 @@ VALUES ('Treasure Pile',
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/DI/Card_Terrain_Treasure_Pile_2.0_DI.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Terrain/Card_Terrain_Back_2.0.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Treasure_Pile_DI.xhtml', 
-'Terrain', '2.0 DI', '2.0 Devil''s Island', 'Default', '');
+'Terrain', '2.0 DI', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2010, ''); 
 insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (27, 1105); --Treasure Pile
 
@@ -24181,7 +24181,7 @@ VALUES ('Backpack',
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_Arcade_Symbol_2.0_DI.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_2.0_DI_back.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Arcade_Symbol.xhtml', 
-'Utility', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+'Utility', '2.0', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2011, 'The cards with the symbol above are for use in games of <b>Super Dungeon: Arcade</b>.<br><br>You can download the rules for <b>Super Dungeon: Arcade</b> at www.ninjadivision.com.<br><br>A print version of <b>Super Dungeon: Arcade</b> is available as part of <b>Super Dungeon: Arcade - Rocktop Rumble</br>');
 
 --Basic Actions	--CardIndex 2012 --UtilityIndex 29
@@ -24190,7 +24190,7 @@ VALUES ('Basic Actions',
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_Basic_Actions_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_2.0_DI_back.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Basic_Actions.xhtml', 
-'Utility', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+'Utility', '2.0', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2012, 'BASIC ACTIONS pg. 19 <br>Magic Attack: Magic, Make an offensive action using WILL.</br><br><br>Melee Attack: Melee, Make an offensive action using STR.</br><br><br>Missile Attack: Missile, Make an offensive action using DEX.</br><br><br>Dash: Move to squares.</br><br><br>Open Chest: Open an adjacent Treasure Chest.</br><br><br>Interact: Augment, The model interacts with an element of the scenario as described in the scenario.</br><br><br>Loud Slurping: Augment, the Hero gains 2 wrath tokens.</br><br><br>POTIONS pg. 18</br><br>Once per turn, a Hero may discard a potion token to take a potion action without spending action points.</br><br><br>Blue Potion: Make the listed support action.</br><br><br>Red Potion: Make the listed offensive action.</br><br><br>Green Potion: Make the listed support action. Green potions may only be used during the Consul''s turn.</br>');
 
 --Basic Actions	DI --CardIndex 2013 --UtilityIndex 30
@@ -24199,7 +24199,7 @@ VALUES ('Basic Actions',
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_Basic_Actions_2.0_DI.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_2.0_DI_back.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Basic_Actions_DI.xhtml', 
-'Utility', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+'Utility', '2.0', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2013, 'BASIC ACTIONS pg. 19 <br>Magic Attack: Magic, Make an offensive action using WILL.</br><br><br>Melee Attack: Melee, Make an offensive action using STR.</br><br><br>Missile Attack: Missile, Make an offensive action using DEX.</br><br><br>Dash: Move to squares.</br><br><br>Open Chest: Open an adjacent Treasure Chest.</br><br><br>POTIONS pg. 18</br><br>Once per turn, a Hero may discard a potion token to take a potion action without spending action points.</br><br><br>Blue Potion: Make the listed support action.</br><br><br>Red Potion: Make the listed offensive action.</br><br><br>Green Potion: Make the listed support action. Green potions may only be used during the Consul''s turn.</br>');
 
 --Basic Actions DI back	--CardIndex 2014 --UtilityIndex 31
@@ -24208,7 +24208,7 @@ VALUES ('Basic Actions DI back',
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_Basic_Actions_2.0_DI_back.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_2.0_DI_back.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Basic_Actions_DI_back.xhtml', 
-'Utility', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+'Utility', '2.0', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2014, 'HERO TURN SUMMARY pg. 13</br><br>Perform the following:</br><br><br>Complete one Hero activation</br><br>Complete a second Hero activation</br><br>Power-Up Phase</br><br><br>HERO ACTIVATION pg. 14</br><br>When activating a Hero perform the following:</br><br>Add an activation token to the Hero''s card</br><br>Upkeep</br><br>Movement and Actions - in any order and combination</br><br><br> HERO POWER-UP pg. 20</br><br>Perform the following during Power-Up:</br><br>Rewards, pg. 20</br><br>Princess Coins, pg. 21</br><br>Equipment</br><br><br>TYPES OF ACTIONS pg 15</br><br>Red offensive actions inflict a single wound if successful.</br><br><br>Blue support actions do not inflict wounds, instead they will list other effects.</br><br><br>Augment actions only affect friendly models. Augment actions that do not list a range affect the model using the action.</br><br><br>Dangerous actions will affect both friendly and enemy models.</br>');
 
 --Dice Conversion DI	--CardIndex 2015 --UtilityIndex 32
@@ -24217,7 +24217,7 @@ VALUES ('Dice Conversion DI',
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_Dice_Conversion_2.0_DI.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_2.0_DI_back.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Dice_Conversion_DI.xhtml', 
-'Utility', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+'Utility', '2.0', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2015, 'DICE CONVERSION</br><br><br>If you do not have Super Dungeon dice, you can instead use three colors of standard six-sided dice. Below is a conversion chart for using standard dice until you get a set of Super Dungeon dice!</br><br><br></br>');
 
 --Hero Turn Summary	--CardIndex 2016 --UtilityIndex 33
@@ -24226,7 +24226,7 @@ VALUES ('Hero Turn Summary',
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_Hero_Turn_Summary_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_2.0_DI_back.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Hero_Turn_Summary.xhtml', 
-'Utility', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+'Utility', '2.0', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2016, 'HERO TURN SUMMARY pg. 13</br><br>Perform one of the following:</br><br>Complete one Hero activation</br><br>Complete a second Hero activation</br><br>Power-Up Phase</br><br><br>HERO ACTIVATION pg. 14</br><br>When activating a Hero perform the following:</br><br>Add an activation token to the Hero''s card</br><br>Upkeep</br><br>Movement and Actions - in any order and combinations</br><br><br>HERO POWER-UP pg. 32</br><br>Perform the following during Power-Up:</br><br>Rewards, pg. 32</br><br>Equipment, pg. 32</br><br>Princess Coins, pg. 33</br><br><br>TYPES OF ACTIONS pg. 19</br><br>Red offensive actions inflict a single wound if successful.</br><br><br>Blue support actions do not inflict wounds, instead they will list other effects.</br><br><br>Augment actions only affect friendly models. Augment actions that do not list a range affect the model using the action.</br><br><br>Dangerous actions will affect both friendly and enemy models.</br>');
 
 --Read Before Opening 1	--CardIndex 2017 --UtilityIndex 34
@@ -24235,7 +24235,7 @@ VALUES ('Read Before Opening 1',
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_Read_Before_Opening_1_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_2.0_DI_back.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Read_Before_Opening_1.xhtml', 
-'Utility', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+'Utility', '2.0', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2017, 'READ BEFORE OPENING!</br><br><br>This pack contains Super Dungeon: Explore game cards for the monster models included in the Super Dungeon: Arcade board game. Like the kobold and drake cards, there are alternate profiles for many of the monsters, providing you with an increased variety of monsters you may choose from to play your games.</br><br><br>A copy of Super Dungeon: Arcade is required to get the models represented by these cards. If you would like to try out these monsters before purchasing Arcade, feel free to pretend your kobold and drake models are the monsters on these cards!</br>');
 
 --Read Before Opening 2	--CardIndex 2018 --UtilityIndex 35
@@ -24244,7 +24244,7 @@ VALUES ('Read Before Opening 2',
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_Read_Before_Opening_2_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_2.0_DI_back.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Read_Before_Opening_2.xhtml', 
-'Utility', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+'Utility', '2.0', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2018, 'READ BEFORE OPENING!</br><br><br>This pack contains alternative profile game cards for the monster models included in the Super Dungeon: Arcade board game. (This box!)</br><br><br>Once you have played games using the standard monster cards, use these cards to increase the variety of monsters you may choose from to play your games.</br>');
 
 --Read Before Opening 3	--CardIndex 2019 --UtilityIndex 36
@@ -24253,7 +24253,7 @@ VALUES ('Read Before Opening 3',
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_Read_Before_Opening_3_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_2.0_DI_back.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Read_Before_Opening_3.xhtml', 
-'Utility', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+'Utility', '2.0', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2019, 'READ BEFORE OPENING!</br><br><br>This pack contains Super Dungeon: Arcade game cards for the monster models included in the Super Dungeon: Explore board game. Like the elf and spider cards, there are alternate profiles for many of the monsters, providing you with an increased variety of monsters you may choose from to play your games.</br><br><br>A copy of Super Dungeon: Explore is required to get the models represented by these cards. If you would like to try out these monsters before purchasing Explore, feel free to pretend your elf and spider models are the monsters on these cards!</br>');
 
 --Read Before Opening 4	--CardIndex 2020 --UtilityIndex 37
@@ -24262,7 +24262,7 @@ VALUES ('Read Before Opening 4',
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_Read_Before_Opening_4_2.0.png', 
 'http://htkb.info/SDE/2.0/Cards/Utility/Card_Utility_2.0_DI_back.png', 
 'http://htkb.online/SDWiki/SDE/2.0/Cards/Utility/Read_Before_Opening_4.xhtml', 
-'Utility', '2.0 Devil''s Island', '2.0 Devil''s Island', 'Default', '');
+'Utility', '2.0', '2.0 DI', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (2020, 'READ BEFORE OPENING!</br><br><br>This pack contains alternative profile game cards for the monster models included in the Super Dungeon: Explore board game. (This box!)</br><br><br>Once you have played games using the standard monster cards, use these cards to increase the variety of monsters you may choose from to play your games.</br>');--2.0 Plot Cards
 --Acid Fog	--CardIndex 2021 --PlotIndex 0
 insert into Cards (Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
