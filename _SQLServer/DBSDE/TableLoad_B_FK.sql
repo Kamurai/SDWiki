a98ab5b588a6ecdb6d849c33e806eea41fa4f4af
Kamurai <kamuraiblue25@gmail.com>
2026-05-03 04:56:11 +0000

    DBSDE: Updated Terrain card links to match SDDirectory

--- _SQLServer/DBSDE/TableLoad_B_FK.sql
+++ _SQLServer/DBSDE/TableLoad_B_FK.sql
@@ -9747,8 +9747,8 @@ insert into KeywordAssignments (EquipmentIndex, KeywordIndex) VALUES (292, 359);
 --Brambles --CardIndex 652 --UtilityIndex 3 --TerrainIndex 2
 insert into Cards (ComponentIndex, Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES (652, 'Brambles',
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Brambles_FK.png', 
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Back_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Brambles_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Back_FK.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Terrain_Cards/Brambles.xhtml', 
 'Terrain', 'FK', 'FK Core Set', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (652, ''); 
@@ -9758,8 +9758,8 @@ insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (3, 209); --D
 --Champion --CardIndex 653 --UtilityIndex 4 --TerrainIndex 3
 insert into Cards (ComponentIndex, Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES (653, 'Champion',
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Champion_FK.png', 
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Back_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Champion_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Back_FK.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Terrain_Cards/Champion.xhtml', 
 'Terrain', 'FK', 'FK Core Set', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (653, ''); 
@@ -9770,8 +9770,8 @@ insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (4, 410); --S
 --Chasm --CardIndex 654 --UtilityIndex 5 --TerrainIndex 4
 insert into Cards (ComponentIndex, Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES (654, 'Chasm',
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Chasm_FK.png', 
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Back_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Chasm_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Back_FK.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Terrain_Cards/Chasm.xhtml', 
 'Terrain', 'FK', 'FK Core Set', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (654, ''); 
@@ -9780,8 +9780,8 @@ insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (5, 196); --C
 --Difficult Terrain --CardIndex 655 --UtilityIndex 6 --TerrainIndex 5
 insert into Cards (ComponentIndex, Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES (655, 'Difficult Terrain',
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Difficult_Terrain_FK.png', 
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Back_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Difficult_Terrain_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Back_FK.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Terrain_Cards/Difficult_Terrain.xhtml', 
 'Terrain', 'FK', 'FK Core Set', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (655, ''); 
@@ -9790,8 +9790,8 @@ insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (6, 209); --D
 --Fresh Grave --CardIndex 656 --UtilityIndex 7 --TerrainIndex 6
 insert into Cards (ComponentIndex, Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES (656, 'Fresh Grave',
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Fresh_Grave_FK.png', 
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Back_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Fresh_Grave_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Back_FK.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Terrain_Cards/Fresh_Grave.xhtml', 
 'Terrain', 'FK', 'FK Core Set', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (656, ''); 
@@ -9800,8 +9800,8 @@ insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (7, 231); --F
 --Goddess --CardIndex 657 --UtilityIndex 8 --TerrainIndex 7
 insert into Cards (ComponentIndex, Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES (657, 'Goddess',
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Goddess_FK.png', 
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Back_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Goddess_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Back_FK.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Terrain_Cards/Goddess.xhtml', 
 'Terrain', 'FK', 'FK Core Set', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (657, ''); 
@@ -9812,8 +9812,8 @@ insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (8, 410); --S
 --Secret Passage --CardIndex 658 --UtilityIndex 9 --TerrainIndex 8
 insert into Cards (ComponentIndex, Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES (658, 'Secret Passage',
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Secret_Passage_FK.png', 
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Back_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Secret_Passage_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Back_FK.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Terrain_Cards/Secret_Passage.xhtml', 
 'Terrain', 'FK', 'FK Core Set', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (658, ''); 
@@ -9822,8 +9822,8 @@ insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (9, 322); --S
 --Sentinel --CardIndex 659 --UtilityIndex 10 --TerrainIndex 9
 insert into Cards (ComponentIndex, Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES (659, 'Sentinel',
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Sentinel_FK.png', 
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Back_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Sentinel_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Back_FK.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Terrain_Cards/Sentinel.xhtml', 
 'Terrain', 'FK', 'FK Core Set', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (659, ''); 
@@ -9834,8 +9834,8 @@ insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (10, 410); --
 --Spawn Location --CardIndex 660 --UtilityIndex 11 --TerrainIndex 10
 insert into Cards (ComponentIndex, Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES (660, 'Spawn Location',
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Spawn_Location_FK.png', 
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Back_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Spawn_Location_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Back_FK.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Terrain_Cards/Spawn_Location.xhtml', 
 'Terrain', 'FK', 'FK Core Set', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (660, ''); 
@@ -9844,8 +9844,8 @@ insert into KeywordAssignments (UtilityIndex, KeywordIndex) VALUES (11, 342); --
 --Structure --CardIndex 661 --UtilityIndex 12 --TerrainIndex 11
 insert into Cards (ComponentIndex, Name, PictureFront, PictureBack, Link, CardType, ProductSet, ProductModule, PlayMode, Flavor)
 VALUES (661, 'Structure',
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Structure_FK.png', 
-'https://htkb.info/ND/SDE/FK/Cards/Utility/Terrain/Card_Terrain_Back_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Structure_FK.png', 
+'https://htkb.info/ND/SDE/FK/Cards/Terrain/Card_Terrain_Back_FK.png', 
 'http://htkb.online/SDWiki/SDE/FK/Cards/Terrain_Cards/Structure.xhtml', 
 'Terrain', 'FK', 'FK Core Set', 'Default', '');
 insert into Utilities (CardIndex, UtilityDescription) VALUES (661, ''); 
